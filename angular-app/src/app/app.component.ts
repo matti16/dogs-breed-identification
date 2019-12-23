ebf22955792c45c1e64eb2cd4e35256fdf851088
matti16 <bmattia92@gmail.com>
2019-12-23 14:34:11 +0100

    remove submodule

---
+++ angular-app/src/app/app.component.ts
@@ -0,0 +1,75 @@
+import {Component, OnInit} from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
+import * as tfd from '@tensorflow/tfjs-data';
+import * as data  from './labels.json';
+import { Tensor } from '@tensorflow/tfjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  public webcam = null;
+  public model: tf.LayersModel = null;
+  public top5Pred = [];
+  public labels = (data as any).default;
+
+  public ngOnInit(): void {
+    this.init();
+  }
+
+  async init(){
+    try {
+      this.webcam = await tfd.webcam(document.getElementById('webcam') as HTMLVideoElement);
+    } catch (e) {
+      console.log(e);
+      document.getElementById('no-webcam').style.display = 'block';
+    }
+    await this.loadModel();
+  
+    await this.predict();
+  }
+
+  async loadModel() {
+    this.model = await tf.loadLayersModel('/assets/models/model.json');
+  }
+
+  /**
+   * Captures a frame from the webcam and normalizes it between -1 and 1.
+   * Returns a batched image (1-element batch) of shape [1, w, h, c].
+   */
+  async getImage() {
+    const img = await this.webcam.capture();
+    const processedImg =
+        tf.tidy(() => img.expandDims(0).toFloat().div(127).sub(1));
+    img.dispose();
+    return processedImg;
+  }
+
+  async predict() {
+
+    while (true) {
+      // Capture the frame from the webcam.
+      const img = await this.getImage();
+  
+      const predictions = this.model.predict(img) as Tensor;
+      const indexedPred = this.zipWithIndex(await predictions.as1D().data());
+      this.top5Pred = indexedPred.sort((a, b) =>  b[1] - a[1]).slice(0, 5);
+      
+      img.dispose();
+      
+      await tf.nextFrame();
+    }
+  }
+
+  zipWithIndex(list) {
+    var result = [];
+    list.forEach((item, index) => {
+        result.push([this.labels[index], item*100]);
+    });
+    return result;
+  }
+
+}
---
+++ angular-app/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+import { HttpClientModule } from  '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatProgressBarModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
